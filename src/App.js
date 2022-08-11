6b7c2c10d7897cd816dfabaa580baa42eae6ab50
ASubby <104353408+ASubby@users.noreply.github.com>
2022-08-11 11:09:46 +0100

    Added github repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Adesubomi and is{" "}
+        <a
+          href="https://github.com/ASubby/weather-project-react"
+          target="_blank"
+        >
+          {" "}
+          Open-sourced{" "}
+        </a>{" "}
+        on github
+      </footer>
+    </div>
+  );
 }
