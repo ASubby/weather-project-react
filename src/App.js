a17f2648f5213de709977bcb2d03eb341a9c28ba
ASubby <104353408+ASubby@users.noreply.github.com>
2022-08-11 11:22:59 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Adesubomi and is{" "}
-        <a
-          href="https://github.com/ASubby/weather-project-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Open-sourced{" "}
-        </a>{" "}
-        on github
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Adesubomi and is{" "}
+          <a
+            href="https://github.com/ASubby/weather-project-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Open-sourced{" "}
+          </a>{" "}
+          on github
+        </footer>
+      </div>
     </div>
   );
 }
